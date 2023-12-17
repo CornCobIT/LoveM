b26f7644a88d6b6916f78615ecf48102b6c9d7c9
20521820 <113028326+20521820@users.noreply.github.com>
2023-12-18 01:25:41 +0700

    Update Login

--- screens/Login.js
+++ screens/Login.js
@@ -1,49 +1,158 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import PageEmail from '../Pages/PageEmail';
-import PagePassword from '../Pages/PagePassword';
-
-const Stack = createNativeStackNavigator();
+import { View, Text, TextInput, StyleSheet, SafeAreaView, TouchableOpacity, Image } from 'react-native'
+import React, { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { firebase } from '../config';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { COLORS } from '../theme/style';
 
 const LoginScreen = () => {
-  
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="PageEmail">
-        <Stack.Screen
-          name="PageEmail"
-          component={PageEmail}
-          options={{
-            title: 'Page Email', //Set Header Title
-            headerStyle: {
-              backgroundColor: 'black', //Set Header color
-            },
-            headerTintColor: '#fff', //Set Header text color
-            headerTitleStyle: {
-              fontWeight: 'bold', //Set Header text style
-            },
-          }}
-        />
-        <Stack.Screen
-          name="PagePassword"
-          component={PagePassword}
-          options={{
-            title: 'Page Password', //Set Header Title
-            headerStyle: {
-              backgroundColor: 'black', //Set Header color
-            },
-            headerTintColor: '#fff', //Set Header text color
-            headerTitleStyle: {
-              fontWeight: 'bold', //Set Header text style
-            },
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
+  const navigation = useNavigation();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
+  loginUser = async (email, password) => {
+    try {
+      await firebase.auth().signInWithEmailAndPassword(email, password)
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <View style={styles.pageStyle}>
+          <View style={styles.main}>
+            <Image
+              source={require('../assets/logo.png')}
+              style={styles.imgStyle} />
+            <Text style={styles.titleName}>Login</Text>
+            <View style={styles.sectionStyle}>
+              <Icon name="mail-outline" size={27} style={styles.iconStyle} />
+              <TextInput
+                placeholder="Email"
+                onChangeText={(email) => setEmail(email)}
+                autoCapitalize="none"
+                autoCorrect={false}
+                style={styles.textInput}
+              />
+            </View>
+          </View>
+
+          <View style={styles.sectionStyle}>
+            <Icon
+              name="lock-closed-outline"
+              size={27}
+              style={styles.iconStyle}
+            />
+            <TextInput
+              style={styles.textInput}
+              placeholder="Password"
+              secureTextEntry={true}
+              onChangeText={(password) => setPassword(password)}
+              autoCapitalize='none'
+              autoCorrect={false}
+            />
+          </View>
+
+          <View style={styles.formatButton}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => loginUser(email, password)}>
+              <Text style={styles.textButton}>LOGIN</Text>
+            </TouchableOpacity>
+          </View>
+
+          <TouchableOpacity
+            onPress={() => navigation.navigate('Register')}
+            style={{ marginTop: 20, justifyContent: 'center', alignItems: 'center' }}
+          >
+
+            <Text
+              style={{
+                fontWeight: 'bold',
+                fontSize: 16,
+                color: COLORS.white,
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}>
+              Don't have an account?
+              <Text style={{ color: COLORS.logo}}>Register Now</Text>
+            </Text>
+
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+  },
+  titleName: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'white',
+  },
+  imgStyle: {
+    width: 200,
+    height: 200,
+    borderRadius: 20,
+    marginBottom: 10,
+  },
+  textInput: {
+    width: 300,
+  },
+  iconStyle: {
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  sectionStyle: {
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    borderWidth: 0.5,
+    height: 50,
+    marginTop: 10,
+    borderRadius: 10,
+  },
+  pageStyle: {
+    flexDirection: 'column',
+    margin: 10,
+  },
+  main: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  formatButton: {
+    alignItems: 'center',
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
+    margin: 10,
+    width: 320,
+    height: 50,
+    backgroundColor: COLORS.logo,
+  },
+  textButton: {
+    fontSize: 16,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  textCheckBox: {
+    margin: 10,
+    color: 'white',
+  },
+});
 
 export default LoginScreen;
